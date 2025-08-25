ebbb1ceba7354139d116bf2c650b96e56e31323b
sanjaysk17 <sanjaykumar6369k@gmail.com>
2025-08-25 11:22:03 +0530

    Added a new Files

---
+++ app.py
@@ -0,0 +1,10 @@
+import pickle
+import streamlit as st 
+model=pickle.load(open('model.pkl','rb'))
+scaler=pickle.load(open('scaler.pkl','rb'))
+st.title("Bike Rental Prediction")
+num=st.number_input("Enter a Temperature ")
+num=scaler.transform([[num]])
+op=model.predict(num)
+st.write("Your Bike Rental is",op)
+
